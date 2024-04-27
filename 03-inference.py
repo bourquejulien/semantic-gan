Fixed backprop

03-inference.py
@@ -44,7 +44,7 @@ class Inferencer(object):
         x_data = np.array([datum], dtype=np.float32)
         if self.gpu >= 0:
             x_data = chainer.cuda.to_gpu(x_data, device=self.gpu)
-        x = chainer.Variable(x_data, volatile=False)
+        x = chainer.Variable(x_data)
         label = self._infer(x)
         return img, label
 
@@ -101,15 +101,15 @@ def main():
 
     dataset = PascalVOC2012Dataset('val')
 
-    infer = Inferencer(dataset, model, gpu)
-    for img_file in img_files:
-        img, label = infer.infer_image_file(img_file)
-        out_img = infer.visualize_label(img, label)
+    with chainer.no_backprop_mode():
+        infer = Inferencer(dataset, model, gpu)
+        for img_file in img_files:
+            img, label = infer.infer_image_file(img_file)
+            out_img = infer.visualize_label(img, label)
 
-        out_file = os.path.join(save_dir, os.path.basename(img_file))
-        scipy.misc.imsave(out_file, out_img)
-        print('- out_file: {0}'.format(out_file))
-
+            out_file = os.path.join(save_dir, os.path.basename(img_file))
+            scipy.misc.imsave(out_file, out_img)
+            print('- out_file: {0}'.format(out_file))
 
 if __name__ == '__main__':
     main()

updater.py
@@ -49,15 +49,15 @@ class GANUpdater(chainer.training.StandardUpdater, UpdaterMixin):
 
     def _get_loss_dis(self):
         batchsize = self.y_fake.data.shape[0]
-        loss = F.softmax_cross_entropy(self.y_real, Variable(self.xp.ones(batchsize, dtype=self.xp.int32), volatile=not self.gen.train))
-        loss += F.softmax_cross_entropy(self.y_fake, Variable(self.xp.zeros(batchsize, dtype=self.xp.int32), volatile=not self.gen.train))
+        loss = F.softmax_cross_entropy(self.y_real, Variable(self.xp.ones(batchsize, dtype=self.xp.int32)))
+        loss += F.softmax_cross_entropy(self.y_fake, Variable(self.xp.zeros(batchsize, dtype=self.xp.int32)))
         chainer.report({'loss': loss}, self.dis)
         return loss
 
     def _get_loss_gen(self):
         batchsize = self.y_fake.data.shape[0]
         L_mce = F.softmax_cross_entropy(self.pred_label_map, self.ground_truth, normalize=False)
-        L_bce = F.softmax_cross_entropy(self.y_fake, Variable(self.xp.ones(batchsize, dtype=self.xp.int32), volatile=not self.gen.train))
+        L_bce = F.softmax_cross_entropy(self.y_fake, Variable(self.xp.ones(batchsize, dtype=self.xp.int32)))
         loss = L_mce + self.L_bce_weight * L_bce
 
         # log report
@@ -99,15 +99,15 @@ class GANUpdater(chainer.training.StandardUpdater, UpdaterMixin):
             else:
                 onehot = np.concatenate([onehot, mask]) 
         return onehot.astype(self.xp.float32)
-
-    def forward(self, batch):
+    
+    def _forward(self, batch):
         label_onehot_batch = [self._onehot_encode(pair[1]) for pair in batch]
 
         input_img, ground_truth = self.converter(batch, self.device)
         ground_truth_onehot = self.converter(label_onehot_batch, self.device)
-        input_img = Variable(input_img, volatile=not self.gen.train)
-        ground_truth = Variable(ground_truth, volatile=not self.gen.train)
-        ground_truth_onehot = Variable(ground_truth_onehot, volatile=not self.gen.train)
+        input_img = Variable(input_img)
+        ground_truth = Variable(ground_truth)
+        ground_truth_onehot = Variable(ground_truth_onehot)
         
         x_real = self._make_dis_input(input_img, ground_truth_onehot)
         y_real = self.dis(x_real)
@@ -120,10 +120,22 @@ class GANUpdater(chainer.training.StandardUpdater, UpdaterMixin):
         self.y_real = y_real
         self.pred_label_map = pred_label_map
         self.ground_truth = ground_truth
+
+    def forward(self, batch):
+        if self.gen.train:
+            self._forward(self, batch)
+        else:
+            with chainer.no_backprop_mode():
+                self._forward(self, batch)
         
     def calc_loss(self):
-        self.loss_dis = self._get_loss_dis()
-        self.loss_gen = self._get_loss_gen()
+        if self.gen.train:
+            self.loss_dis = self._get_loss_dis()
+            self.loss_gen = self._get_loss_gen()
+        else:
+            with chainer.no_backprop_mode():
+                self.loss_dis = self._get_loss_dis()
+                self.loss_gen = self._get_loss_gen()
         
     def backprop(self):
         self.dis.cleargrads()
@@ -149,11 +161,20 @@ class NonAdversarialUpdater(chainer.training.StandardUpdater, UpdaterMixin):
         kwargs = self._standard_updater_kwargs(**kwargs)
         super(NonAdversarialUpdater, self).__init__(*args, **kwargs)
 
-    def forward(self, batch):
+    def _forward(self, batch):
         input_img, ground_truth = self.converter(batch, self.device)
-        input_img = Variable(input_img, volatile=not self.model.train)
-        self.ground_truth = Variable(ground_truth, volatile=not self.model.train)
+        input_img = Variable(input_img)
+        self.ground_truth = Variable(ground_truth)
         self.pred_label_map = self.model(input_img)
+
+
+    def forward(self, batch):
+        if self.gen.train:
+            self._forward(self, batch)
+        else:
+            with chainer.no_backprop_mode():
+                self._forward(self, batch)
+
         
     def calc_loss(self):
         batchsize = self.ground_truth.shape[0]